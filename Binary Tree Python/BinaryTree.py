9af78be3e1653cb6e5856197cb6a8644dedf8dd2
Fahim <fahim.hasnain@ixorasolution.com>
2021-09-13 11:13:07 +0600

    Binary tree more 5 functions

--- Binary Tree Python/BinaryTree.py
+++ Binary Tree Python/BinaryTree.py
@@ -1,4 +1,5 @@
 from Node import Node
+from queue import LifoQueue, Queue
 
 
 class BinaryTree:
@@ -69,6 +70,98 @@ class BinaryTree:
             self.print_same_level(node, i)
             print()
 
+    def print_all_level_at_n(self, node):
+        if node is None:
+            return
+
+        q = [node]
+        while q.__len__() > 0:
+            dnode = q.pop(0)
+            print(dnode.value, end=" ")
+            if dnode.left is not None:
+                q.append(dnode.left)
+
+            if dnode.right is not None:
+                q.append(dnode.right)
+
+    def print_reverse_level(self, node):
+        if node is None:
+            return
+
+        h = self.height(node)
+        for i in range(h, -1, -1):
+            self.print_same_level(node, i)
+            print()
+
+    def print_reverse_level_Stack(self, node):
+        if node is None:
+            return
+        q = Queue(maxsize=0)
+        stack = LifoQueue(maxsize=0)
+
+        q.put(node)
+        while not q.empty():
+            el = q.get()
+
+            stack.put(el)
+            if el.right is not None:
+                q.put(el.right)
+            if el.left is not None:
+                q.put(el.left)
+
+        while not stack.empty():
+            print(stack.get().value, end=" ")
+
+    def print_all_levels_newline(self, node):
+        if node is None:
+            return
+        q = Queue(maxsize=0)
+        s = LifoQueue(maxsize=0)
+        c = self.height(node) + 1
+        q.put(node)
+
+        while True:
+            count = q.qsize()
+            if count == 0:
+                break
+
+            while count > 0:
+                el = q.get()
+                print(el.value, end=" ")
+                if el.left is not None:
+                    q.put(el.left)
+                if el.right is not None:
+                    q.put(el.right)
+
+                count = count - 1
+
+            print()
+
+    maxlevel = 0
+
+    def print_left_view(self, node, level):
+        if node is None:
+            return
+
+        if level >= self.maxlevel:
+            print(node.value, end=" ")
+            self.maxlevel = self.maxlevel + 1
+
+        self.print_left_view(node.left, level + 1)
+        self.print_left_view(node.right, level + 1)
+
+    def print_right_view(self, node, level):
+        if node is None:
+            return
+
+        if level >= self.maxlevel:
+            print(node.value, end=" ")
+            self.maxlevel = self.maxlevel + 1
+
+        self.print_right_view(node.right, level + 1)
+        self.print_right_view(node.left, level + 1)
+
+
 
 if __name__ == '__main__':
     tree = BinaryTree()
@@ -79,8 +172,14 @@ if __name__ == '__main__':
     tree.root.left.right = Node(10)
     tree.root.right.left = Node(8)
     tree.root.right.right = Node(14)
-    print(tree.sum_of_nodes(tree.root))
-    print(tree.num_leaf_nodes(tree.root))
-    print(tree.height(tree.root))
-    print((tree.print_same_level(tree.root, 2)))
-    print(tree.print_all_level(tree.root))
+    # print(tree.sum_of_nodes(tree.root))
+    # print(tree.num_leaf_nodes(tree.root))
+    # print(tree.height(tree.root))
+    # print((tree.print_same_level(tree.root, 2)))
+    # tree.print_all_level(tree.root)
+    # tree.print_all_level_at_n(tree.root)
+    # tree.print_reverse_level(tree.root)
+    # tree.print_reverse_level_Stack(tree.root)
+    tree.print_all_levels_newline(tree.root)
+    # tree.print_left_view(tree.root, 0)
+    tree.print_right_view(tree.root, 0)
