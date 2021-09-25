3c2a296c93fe2cfb230e669b622415015ba3289b
Fahim <fahim.hasnain@ixorasolution.com>
2021-09-25 12:55:56 +0600

    First non-repeating char in online stream

--- Queue_Python/first_non_repeating.py
+++ Queue_Python/first_non_repeating.py
@@ -4,27 +4,31 @@ from Queue import QueuePersonal
 def first_non_repeating(string_inp: str) -> str:
     if string_inp == '':
         return ''
-    str_map = {}
+    count = {}
     q = QueuePersonal()
     last_repeating = string_inp[0]
     ans_str = ""
 
     for i in string_inp:
-        if i in str_map:
-            str_map[i] = str_map[i] + 1
-            if i == last_repeating:
-                ans_str += "-1"
-            else:
-                ans_str = ans_str + last_repeating
-        else:
-            str_map[i] = 1
-            if str_map[last_repeating] > 1:
-                last_repeating = i
-            ans_str = ans_str + last_repeating
+        if i not in count:
+            count[i] = 1
             q.push(i)
+        else:
+            count[i] = count[i] + 1
+
+        while not q.isEmpty():
+            temp = q.top()
+            if count[temp] == 1:
+                ans_str += temp
+                break
+            q.pop()
+        if q.isEmpty():
+            ans_str += "X"
+
     return ans_str
 
 
 if __name__ == '__main__':
-    string = "aabbbcddddddce"
+    string = "aabccdba"
+    # string = "aba"
     print(first_non_repeating(string))
