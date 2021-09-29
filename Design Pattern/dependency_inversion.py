e41a8a2a157914057ab13861e1a505ca1362ce7f
Fahim <fahim.hasnain@ixorasolution.com>
2021-09-29 10:22:26 +0600

    Dependency Inversion Principle

--- Design Pattern/dependency_inversion.py
+++ Design Pattern/dependency_inversion.py
@@ -1,4 +1,17 @@
-class LightBulb:
+from abc import ABC, abstractmethod
+
+
+class Switchable(ABC):
+    @abstractmethod
+    def turn_on(self):
+        pass
+
+    @abstractmethod
+    def turn_off(self):
+        pass
+
+
+class LightBulb(Switchable):
     def turn_on(self):
         print("Light bulb turned on .... ")
 
@@ -6,23 +19,32 @@ class LightBulb:
         print("Light bulb turned off .... ")
 
 
+class Fan(Switchable):
+    def turn_on(self):
+        print('Fan - Turned on....')
+
+    def turn_off(self):
+        print("Fan:  turned off ..... ")
+
+
 class ElectricPowerSwitch:
 
-    def __init__(self, l: LightBulb):
-        self.lightbulb = l
+    def __init__(self, c: Switchable):
+        self.client = c
         self.on = False
 
     def press(self):
         if self.on:
-            self.lightbulb.turn_off()
+            self.client.turn_off()
             self.on = False
         else:
-            self.lightbulb.turn_on()
+            self.client.turn_on()
             self.on = True
 
 
 if __name__ == '__main__':
     l = LightBulb()
-    switch = ElectricPowerSwitch(l)
+    f = Fan()
+    switch = ElectricPowerSwitch(f)
     switch.press()
     switch.press()
