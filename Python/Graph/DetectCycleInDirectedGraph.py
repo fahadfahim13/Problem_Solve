66c1a96208d5e9c7755b9703ab17aaea560136bc
Fahim <fahim.hasnain@ixorasolution.com>
2021-10-11 09:59:29 +0600

    detect cycle

--- Python/Graph/DetectCycleInDirectedGraph.py
+++ Python/Graph/DetectCycleInDirectedGraph.py
@@ -8,13 +8,30 @@ def detect_cycle_directed_graph(graph: list[list[int]], num_of_nodes: int) -> bo
         i = stack[-1]
         if visited[i] == 0:
             visited[i] = 1
-            new_node_found = False
-            for j in graph[i]:
-                if visited[j] and j in stack:
-                    return True
-                elif visited[j] == 0:
-                    new_node_found = True
-                    stack.append(j)
-                    break
-            if not new_node_found:
-                stack.pop()
+        new_node_found = False
+        for j in graph[i]:
+            if visited[j] and j in stack:
+                return True
+            elif visited[j] == 0:
+                new_node_found = True
+                stack.append(j)
+                break
+        if not new_node_found:
+            stack.pop()
+    return False
+
+
+def main():
+    # arr = [[1, 2], [2, 3], [2, 4], [3, 4]]
+    arr = [
+        [1, 2],
+        [3],
+        [],
+        []
+    ]
+    n = 4
+    print(detect_cycle_directed_graph(arr, n))
+
+
+if __name__ == "__main__":
+    main()
