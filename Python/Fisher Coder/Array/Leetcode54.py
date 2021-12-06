3b31981a8081ccbac91c816597917b08d9cc419d
Fahim <fahim.hasnain@ixorasolution.com>
2021-12-06 07:29:03 +0600

    Spiral matrix done

--- Python/Fisher Coder/Array/Leetcode54.py
+++ Python/Fisher Coder/Array/Leetcode54.py
@@ -1,13 +1,47 @@
 def spiralMatrix(nums: list[list[int]]) -> list[int]:
-    m = len(nums[0])
-    n = len(nums)
+    if nums is None or len(nums) == 0 or len(nums[0]) == 0:
+        return []
+    n = len(nums[0])
+    m = len(nums)
+    top = 0
+    bottom = m - 1
+    left = 0
+    right = n - 1
     ans_arr = []
+
+    while len(ans_arr) < m * n:
+        for j in range(left, right + 1):
+            if len(ans_arr) < m * n:
+                ans_arr.append(nums[top][j])
+            else:
+                break
+        top += 1
+        for j in range(top, bottom + 1):
+            if len(ans_arr) < m * n:
+                ans_arr.append(nums[j][right])
+            else:
+                break
+        right -= 1
+        for j in range(right, left - 1, -1):
+            if len(ans_arr) < m * n:
+                ans_arr.append(nums[bottom][j])
+            else:
+                break
+        bottom -= 1
+
+        for j in range(bottom, top - 1, -1):
+            if len(ans_arr) < m * n:
+                ans_arr.append(nums[j][left])
+            else:
+                break
+        left += 1
 
     return ans_arr
 
 
 def main():
-    arr = [[1,2,3],[4,5,6],[7,8,9]]
+    # arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    arr = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
     print(spiralMatrix(arr))
 
 
--- Python/Fisher Coder/Array/Leetcode59.py
+++ Python/Fisher Coder/Array/Leetcode59.py
@@ -1,9 +1,34 @@
-def generateSpiralMatrix(num: int) -> list[int]:
-    pass
+def generateSpiralMatrix(n: int) -> list[list[int]]:
+    if n <= 0:
+        return [[]]
+    current = 1
+    left = 0
+    right = n - 1
+    top = 0
+    bottom = n - 1
+    ans_arr = [[0 for i in range(n)] for j in range(n)]
+    while left <= right and top <= bottom:
+        for i in range(left, right + 1):
+            ans_arr[top][i] = current
+            current += 1
+        top += 1
+        for j in range(top, bottom + 1):
+            ans_arr[j][right] = current
+            current += 1
+        right -= 1
+        for k in range(right, left - 1, -1):
+            ans_arr[bottom][k] = current
+            current += 1
+        bottom -= 1
+        for l in range(bottom, top - 1, -1):
+            ans_arr[l][left] = current
+            current += 1
+        left += 1
+    return ans_arr
 
 
 def main():
-    n = 5
+    n = 4
     print(generateSpiralMatrix(n))
 
 
